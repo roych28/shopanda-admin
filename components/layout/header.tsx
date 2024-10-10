a5f0d6c7ef13e8fe2b6c53c39b2588539e5d092e
roych28 <roych28@gmail.com>
2024-10-10 17:57:30 +0300

    header

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -9,9 +9,9 @@ export default function Header() {
     <header className="sticky inset-x-0 top-0 w-full">
       <nav className="flex items-center justify-between px-4 py-2 md:justify-end">
       <div className={cn('block lg:!hidden flex items-center space-x-6')}>
-        <ThemeToggle />
-        <div/>
         <UserNav />
+        <div/>
+        <ThemeToggle />
       </div>
         <div className="flex items-center gap-2">
           <div className="justify-self" >
