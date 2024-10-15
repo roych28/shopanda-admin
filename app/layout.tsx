28e4bc4744df8d1bf7318132fa2330206369f1b1
roych28 <roych28@gmail.com>
2024-10-15 03:28:57 +0300

    favicon png

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,6 +30,11 @@ export default async function RootLayout({
 
   return (
     <html lang={locale} dir={locale === 'he' ? 'rtl' : 'ltr'}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.png" type="image/png" />
+      </head>
       <body
         className={`${inter.className} overflow-hidden `}
         suppressHydrationWarning={true}
